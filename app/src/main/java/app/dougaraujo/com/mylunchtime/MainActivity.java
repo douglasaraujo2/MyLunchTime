6e404829db5ad8a6c41791165f3523c15477eeb7
Douglas Araujo <douglas@duxinnovation.com>
2017-08-03 16:33:30 -0300

    Nome do usuário

--- app/src/main/java/app/dougaraujo/com/mylunchtime/MainActivity.java
+++ app/src/main/java/app/dougaraujo/com/mylunchtime/MainActivity.java
@@ -2,9 +2,8 @@ package app.dougaraujo.com.mylunchtime;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,10 +11,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private TextView tvUsername;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +41,11 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+        tvUsername = (TextView) header.findViewById(R.id.tvUsername);
+        String usuario = LoginActivity.getDefault("login", this);
+        tvUsername.setText(usuario);
     }
-
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
