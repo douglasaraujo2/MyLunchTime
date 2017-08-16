6acefb3336408949f3d36f0427602929ae4db936
Douglas Araujo <douglas@duxinnovation.com>
2017-08-16 10:38:14 -0300

    Criando cardview

--- app/src/main/java/app/dougaraujo/com/mylunchtime/MainActivity.java
+++ app/src/main/java/app/dougaraujo/com/mylunchtime/MainActivity.java
@@ -88,17 +88,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.navList) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.navNew) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.navMap) {
 
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         } else if (id == R.id.navLogOut) {
             SharedPreferences pref = getSharedPreferences("info", MODE_PRIVATE);
