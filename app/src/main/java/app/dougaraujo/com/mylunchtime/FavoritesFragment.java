427879693a67fb85cbca91a68f5146cc088ab6c2
Douglas Araujo <douglas@duxinnovation.com>
2017-08-23 10:29:10 -0300

    Botão editar e deletar aparecendo

--- app/src/main/java/app/dougaraujo/com/mylunchtime/FavoritesFragment.java
+++ app/src/main/java/app/dougaraujo/com/mylunchtime/FavoritesFragment.java
@@ -1,11 +1,11 @@
 package app.dougaraujo.com.mylunchtime;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +15,7 @@ import java.util.List;
 
 import app.dougaraujo.com.mylunchtime.DAO.FavoriteDAO;
 import app.dougaraujo.com.mylunchtime.adapter.FavoriteAdapter;
+import app.dougaraujo.com.mylunchtime.adapter.SwipeHelper;
 import app.dougaraujo.com.mylunchtime.model.Favorite;
 
 /**
@@ -22,25 +23,27 @@ import app.dougaraujo.com.mylunchtime.model.Favorite;
  */
 public class FavoritesFragment extends Fragment {
     RecyclerView rvFavorites;
-    ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-        @Override
-        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-            return false;
-        }
+//    ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+//        @Override
+//        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+//            return false;
+//        }
+//
+//        @Override
+//        public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+//            //Remove swiped item from list and notify the RecyclerView
+//            Toast.makeText(getActivity(), new String(String.valueOf(swipeDir)), Toast.LENGTH_SHORT).show();
+//        }
+//    };
+//    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
 
-        @Override
-        public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
-            //Remove swiped item from list and notify the RecyclerView
-            Toast.makeText(getActivity(), new String(String.valueOf(swipeDir)), Toast.LENGTH_SHORT).show();
-        }
-    };
-    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
     private FavoriteAdapter favoriteAdapter;
     private List<Favorite> favorites;
 
     public FavoritesFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,15 +54,48 @@ public class FavoritesFragment extends Fragment {
         favorites = favoriteDAO.getAll();
         favoriteAdapter = new FavoriteAdapter(favorites);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        itemTouchHelper.attachToRecyclerView(rvFavorites);
+//;        itemTouchHelper.attachToRecyclerView(rvFavorites);
         rvFavorites.setLayoutManager(layoutManager);
         rvFavorites.setAdapter(favoriteAdapter);
         rvFavorites.setHasFixedSize(true);
-
         favoriteAdapter.update(favorites);
+        initSwipe();
         return itemView;
 
     }
 
+    private void initSwipe() {
+        SwipeHelper swipeHelper = new SwipeHelper(getActivity(), rvFavorites) {
+            @Override
+            public void instantiateUnderlayButton(RecyclerView.ViewHolder viewHolder, List<UnderlayButton> underlayButtons) {
+                underlayButtons.add(new SwipeHelper.UnderlayButton(
+                        getResources().getString(R.string.txt_delete),
+                        0,
+                        Color.parseColor(String.valueOf(R.color.btnDelete)),
+                        new SwipeHelper.UnderlayButtonClickListener() {
+                            @Override
+                            public void onClick(int pos) {
+                                Toast.makeText(getActivity(), "Clicou no delete", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                ));
+
+                underlayButtons.add(new SwipeHelper.UnderlayButton(
+                        getResources().getString(R.string.txt_edit),
+                        0,
+                        Color.parseColor(String.valueOf(R.color.btnEdit)),
+                        new SwipeHelper.UnderlayButtonClickListener() {
+                            @Override
+                            public void onClick(int pos) {
+                                Toast.makeText(getActivity(), "Clicou no edit", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                ));
+            }
+        };
+    }
+
 
 }
+
+//https://maps.googleapis.com/maps/api/geocode/json?address=Avenida%20Prefeito%20Paulo%20Lauro,264&key=
