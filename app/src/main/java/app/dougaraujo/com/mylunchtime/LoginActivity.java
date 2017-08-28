687692f9d3feb9ec0d2dbbdfe752885e5e8f6945
Douglas Araujo <douglas@duxinnovation.com>
2017-08-28 18:26:23 -0300

    Login com facebook quase ok

--- app/src/main/java/app/dougaraujo/com/mylunchtime/LoginActivity.java
+++ app/src/main/java/app/dougaraujo/com/mylunchtime/LoginActivity.java
@@ -2,7 +2,9 @@ package app.dougaraujo.com.mylunchtime;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,11 +16,14 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-import java.util.Arrays;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import app.dougaraujo.com.mylunchtime.DAO.UsuarioDAO;
 import app.dougaraujo.com.mylunchtime.model.Usuario;
@@ -32,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
     private LoginButton loginButton;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.FROYO)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,13 +50,38 @@ public class LoginActivity extends AppCompatActivity {
         isKeep = (CheckBox) findViewById(R.id.cbKeep);
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions(Arrays.asList(
-                "public_profile", "email"));
+        loginButton.setReadPermissions("email");
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+
+
+                                // Application code
+                                try {
+                                    String email = object.getString("email");
+                                    String nome = object.getString("name");
+                                    SharedPreferences pref = getSharedPreferences("info", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = pref.edit();
+                                    editor.putString("username", nome);
+
+                                    editor.apply();
+                                    navegarViewPrincipal();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
 
             @Override
